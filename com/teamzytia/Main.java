d8f05b185722f5809ecb6ec153aefb48b81eb1f7
Matthew Hogan <matt@matthogan.co.uk>
2014-11-19 19:46:47 +0000

    Update Main.java

--- com/teamzytia/Main.java
+++ com/teamzytia/Main.java
@@ -1,10 +1,21 @@
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-public class Main extends JavaPlugin{ 
-
-public void onEnable() {
+public class Main extends JavaPlugin { 
   
-}
+  private static Plugin instance;
 
+  @Override
+  public void onEnable() {
+    instance = this;
+  }
+
+  @Override
+  public void onDisable() {
+    instance = null;
+  }
+  
+  /**
+  * @return The Bukkit Plugin instance
+  */
+  public static Plugin getInstance() { return instance; } 
 }
