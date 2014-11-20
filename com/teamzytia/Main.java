b8ba8e4bd1f33268b2cbc3602d4a4edb57ade84b
Matthew Hogan <matt@matthogan.co.uk>
2014-11-20 17:41:56 +0000

    reverts back to an older version

--- com/teamzytia/Main.java
+++ com/teamzytia/Main.java
@@ -1,21 +1,8 @@
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin { 
-  
-  private static Plugin instance;
-
-  @Override
-  public void onEnable() {
-    instance = this;
-  }
-
-  @Override
-  public void onDisable() {
-    instance = null;
-  }
-  
-  /**
-  * @return The Bukkit Plugin instance
-  */
-  public static Plugin getInstance() { return instance; } 
+public class Main extends JavaPlugin{ 
++
+public void onEnable() {
+   
+}
 }
